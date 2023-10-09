3d07fb442c9c1e67c1d498a3943f22aca6c650bd
KalyanGupta <kalyannarla1843@gmail.com>
2023-10-09 16:16:36 +0530

    Episode 1 code

---
+++ App.js
@@ -0,0 +1,48 @@
+/*
+Required Structure:
+
+<div id=”parent”>
+
+    <div id=”child”>
+        <h1 id="heading">I am h1 tag </h1>
+        <h2 id="heading">I am h2 tag </h2>
+    </div>
+
+    <div id=”child1”>
+        <h1 id="heading">I am h1 tag child1</h1>
+        <h2 id="heading">I am h2 tag child2</h2>
+    </div>
+
+</div>
+
+*/
+
+const parent = React.createElement(
+    "div", 
+    {id: "parent"},
+    [
+        React.createElement(
+            "div",
+            {id:"child"},
+            [ React.createElement("h1", {id:"heading"}, "I am h1 tag"), 
+            React.createElement("h2", {id:"heading"}, "I am h2 tag") ]
+
+        ), 
+        
+        React.createElement(
+            "div",
+            {id:"child1"},
+            [ React.createElement("h1", {id:"heading"}, "I am h1 tag child1"), 
+            React.createElement("h2", {id:"heading"}, "I am h2 tag child1") ]
+
+        ), 
+
+
+    ]
+)    
+console.log(parent); //object
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
+
+
